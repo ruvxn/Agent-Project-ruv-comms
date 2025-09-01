bd9d9d2349fba58f7bed14eec6ff7779e4b12b10
vold <email@example.com>
2025-09-01 12:35:01 +1000

    updated testing

--- langgraph_test.py
+++ langgraph_test.py
@@ -1,11 +1,14 @@
 from langgraph.prebuilt import create_react_agent
+from langgraph.store.sqlite import SqliteStore
 from langgraph.store.memory import InMemoryStore
+import sqlite3
 from langmem import create_manage_memory_tool, create_search_memory_tool
 def get_weather(city: str) -> str:
     """Get Weather for a given city"""
     return f"Its always sunny {city}!"
-
-store = InMemoryStore(
+conn = sqlite3.connect('databse.sqlite')
+store = SqliteStore(
+    conn=conn,
     index={
         "dims": 1536,
          "embed": "openai:text-embedding-3-small",
@@ -23,14 +26,15 @@ agent = create_react_agent(
 )
 
 while True:
-    print("enter your request")
+    print("Enter your request")
     user_input = input()
+    print(f'User: {user_input}')
     if user_input:
         reply = agent.invoke(
             {"messages": [{"role": "user", "content": user_input}]}
         )
         user_input = ""
-        print(reply["messages"][-1].content)
+        print(f'Agent: {reply["messages"][-1].content}')
 
 
 
