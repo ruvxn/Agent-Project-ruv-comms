ee04cf593ff70ff675312a91ca97d5fad776c680
atom300 <a.safdari23@pm.me>
2025-09-09 08:44:07 +1000

    added new tool

--- Agent.py
+++ Agent.py
@@ -1,6 +1,6 @@
 from AgentState import State
 from langgraph.graph import StateGraph, START, END
-from tool import WebSearch
+from tool import WebSearch, WebScrape
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.prebuilt import ToolNode
@@ -10,8 +10,9 @@ class Agent:
     """An Agent class"""
     def __init__(self):
         self.llm = init_chat_model("openai:gpt-4o-mini")
-        self.tool = WebSearch()
-        self.tools = [self.tool]
+        self.tool_search = WebSearch()
+        self.tool_scrape = WebSearch()
+        self.tools = [self.tool_search, self.tool_scrape]
         self.llm_with_tools  = self.llm.bind_tools(self.tools)
     def chat(self, state: State):
         return {"messages":[self.llm_with_tools.invoke(state["messages"])]}
@@ -30,7 +31,7 @@ class Agent:
         graph_builder = StateGraph(State)
         graph_builder.add_node("chatbot", self.chat)
 
-        tool_node = ToolNode(tools=[self.tool])
+        tool_node = ToolNode(tools=[self.tool_search, self.tool_scrape])
         graph_builder.add_node("tools", tool_node)
 
         graph_builder.add_conditional_edges(
--- tool.py
+++ tool.py
@@ -1,8 +1,25 @@
 from langchain_core.tools import BaseTool
-import requests
-import selenium
 from ddgs import DDGS
 import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+class WebScrape(BaseTool):
+    """A tool that scrapes a website given the link"""
+    name: str = "WebScrape"
+    description: str = "A tool that can scrape a website given a specific link"
+    def _run(self, link: str) -> str:
+        return "Link is broken at the moment"
+        driver = webdriver.Firefox()
+        try:
+            driver.get(link)
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "BC4Pbi Juh2_H GPUVdP"))
+            )
+            return json.dumps(element, indent=2)
+        except Exception as e:
+            return f'An error occured during scraping: {e}'
 class WebSearch(BaseTool):
     """Tool class that inherits from base tool"""
     name: str = "WebSearch"
