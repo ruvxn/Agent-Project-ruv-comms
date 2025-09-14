928d1208ffcd2e2d8d03306b91c436cd48d22fdc
field845 <845030043@qq.com>
2025-09-14 10:38:15 +1000

    fix(app.py): delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-from Controller.utils import load_data_to_raw_review_table, chunking
-from Controller.db_setup.mysql_setup import get_raw_review_data_by_id
-
-result = get_raw_review_data_by_id("22287554-10b8-4d0b-8370-811078cfb110")
-
-print(result.data["review_title"])
